46b595d69e9b47de6975caa70f64c133833f35a5
wei <wei>
2019-11-01 18:01:38 +0800

    feat:mutiple entrypoint change

---
+++ src/index/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+// import "normalize.css";
+import "./index.css";
+
+import store from "./store";
+import App from "./App.jsx";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+document.querySelectorById('root'));
